67f4733a56ddcc8f4eda066966fefbe6ffc6e801
Monark Basak <monarkb01@gmail.com>
2024-10-01 15:22:08 -0400

    Added dark theme and navigation menu to site.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import Navbar from "./components/Nav";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>
+                <p className="p-10">Testing</p>
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   );
 }
